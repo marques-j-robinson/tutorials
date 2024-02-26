0ea69cdbddab5ea78edeff55d9ab676bdec9bd7c
marques-j-robinson <marques.j.robinson@gmail.com>
2024-02-26 02:18:53 -0800

    modify server.js to utilize pug template engine

--- expressjs/server.js
+++ expressjs/server.js
@@ -1,7 +1,9 @@
 import express from 'express'
 
-const app = express()
 const port = process.env.PORT || 5656
+const app = express()
+
+app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
     res.send('Hello World')
