ef062f7da02c866c7837a3e433c10bfd4b7a3360
marques-j-robinson <marques.j.robinson@gmail.com>
2024-03-07 22:10:19 -0800

    updates to expressjs project

--- expressjs/server.js
+++ expressjs/server.js
@@ -5,8 +5,20 @@ const app = express()
 
 app.set('view engine', 'pug')
 
-app.get('/', (req, res) => {
+app.get('/charles', (req, res) => {
     res.render('index')
+})
+
+app.get('/marilyn', (req, res) => {
+    res.render('index')
+})
+
+app.get('/jean', (req, res) => {
+    res.render('index')
+})
+
+app.get('/', (req, res) => {
+    res.redirect('/charles')
 })
 
 app.listen(port, () => {
